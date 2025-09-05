9fb1821b1b0958fd7e6ee7139753c4658b314eba
loup <loup.quehec@gmail.com>
2025-09-05 18:12:08 +0200

    update

--- volumes/functions/main/index.ts
+++ volumes/functions/main/index.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.177.1/http/server.ts'
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
 import * as jose from 'https://deno.land/x/jose@v4.14.4/index.ts'
 
 console.log('main function started')
