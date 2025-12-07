13ee7b115ebfe44e543cb479b3de096210016875
loup <loup.quehec@gmail.com>
2025-12-08 00:52:41 +0100

    fix: move edge function can't handle sitemaps generation

--- volumes/functions/generate-sitemaps/slugify.ts
+++
@@ -1,8 +0,0 @@
-export const slugify = (str: string): string => {
-	if (!str) return "";
-	let s = str.toLowerCase();
-	s = s.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-	s = s.replace(/[^a-z0-9\-_]+/g, "-");
-	s = s.replace(/^-+|-+$/g, "");
-	return s;
-}
